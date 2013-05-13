ec24e02142576c1843834f71945826b94cbf000e
Ben Lowry <benlowry@outlook.com>
2013-05-13 09:59:03 -0600

    Fixed divide by zero and reformatted

--- Assets/Plugins/Playtomic/PlayerLevel.cs
+++ Assets/Plugins/Playtomic/PlayerLevel.cs
@@ -1,124 +1,109 @@
 using System;
 using System.Collections;
 
+//namespace Playtomic
+//{
 public class PlayerLevel : Hashtable
 {
-	public PlayerLevel()
+	public PlayerLevel ()
 	{
 	}
-	
-	public PlayerLevel(Hashtable data)
+		
+	public PlayerLevel (IDictionary data)
 	{
-		foreach(string x in data.Keys)
-		{			
-			if(x == "date") 
-			{
-				var d = new DateTime(1970, 1, 1, 0, 0, 0);
-				this.date = d.AddSeconds ((double)data[x]);
-			} 
-			else 
-			{
-				this[x] = data[x];
+		foreach (string x in data.Keys) {			
+			if (x == "date") {
+				var d = new DateTime (1970, 1, 1, 0, 0, 0);
+				date = d.AddSeconds ((double)data [x]);
+			} else {
+				this [x] = data [x];
 			}
 		}
 	}
-	
-	public string levelid 
-	{
+		
+	public string levelid {
 		get { return GetString ("levelid"); }
 		set { SetProperty ("levelid", value); }
 	}
-	
-	public string playersource
-	{
-		get { return GetString ("playersource"); }
-		set { SetProperty ("playersource", value); }
+		
+	public string source {
+		get { return GetString ("source"); }
+		set { SetProperty ("source", value); }
 	}
-	
-	public string playerid
-	{
+		
+	public string playerid {
 		get { return GetString ("playerid"); }
 		set { SetProperty ("playerid", value); }
 	}
-	
-	public string playername
-	{
+		
+	public string playername {
 		get { return GetString ("playername"); }
 		set { SetProperty ("playername", value); }
 	}
-	
-	public string name
-	{
+		
+	public string name {
 		get { return GetString ("name"); }
 		set { SetProperty ("name", value); }
 	}
-	
-	public string data
-	{
+		
+	public string data {
 		get { return GetString ("data"); }
 		set { SetProperty ("data", value); }
 	}
-	
-	public long votes 
-	{
-		get { return GetLong("votes"); }
-		set { SetProperty ("votes", value); }
+		
+	public long votes {
+		get { return GetLong ("votes"); }
 	}
-	
-	public long plays
-	{
-		get { return GetLong ("plays"); }
-		set { SetProperty ("plays", value); }
-	}
-	
-	public double rating
-	{
-		get { return score / votes; }
-	}
-	
-	public long score 
-	{
+
+	public long score {
 		get { return GetLong ("score"); }
-		set { SetProperty ("score", value); }
-	}
-	
-	public string sdate 
-	{
-		get { return GetString ("sdate"); }
-		set { SetProperty ("sdate", value); }
-	}
-	
-	public DateTime date
-	{
-		get { return this.ContainsKey ("date") ? (DateTime) this["date"] : DateTime.Now; }
-		set { SetProperty ("date", value); }
-	}
-	
-	public Hashtable fields
-	{
-		get { return this.ContainsKey ("fields") ? (Hashtable)this["fields"] : new Hashtable();	}
-		set { SetProperty ("fields", value); }
 	}
 		
-	private long GetLong(string s) 
-	{
-		return this.ContainsKey (s) ? (Int64) this[s] : 0L;
+	public double rating {
+		get { 
+			var s = score;
+			var v = votes;
+
+			if (s == 0 || v == 0) {
+				return 0;
+			}
+
+			return s / v;
+		}
 	}
-	
-	private string GetString(string s) 
+
+	public DateTime date {
+		get { return ContainsKey ("date") ? (DateTime)this ["date"] : DateTime.Now; }
+		set { SetProperty ("date", value); }
+	}
+
+	public string rdate {
+		get { return GetString ("rdate"); }
+		set { SetProperty ("rdate", value); }
+	}
+		
+	public Hashtable fields {
+		get { return ContainsKey ("fields") ? (Hashtable)this ["fields"] : new Hashtable ();	}
+		set { SetProperty ("fields", value); }
+	}
+			
+	private long GetLong (string s)
+	{
+		return ContainsKey (s) ? (Int64)this [s] : 0L;
+	}
+		
+	private string GetString (string s)
 	{	
-		return this.ContainsKey (s) ? this[s].ToString () : null;
+		return ContainsKey (s) ? this [s].ToString () : null;
 	}
-	
-	private void SetProperty(string key, object value) 
+		
+	private void SetProperty (string key, object value)
 	{
-		if(this.ContainsKey(key))
-		{
-			this[key] = value;
-		} 
-		else 
-		{
-			this.Add(key, value);
+		if (ContainsKey (key)) {
+			this [key] = value;
+		} else {
+			Add (key, value);
 		}
 	}
 }
+//}
