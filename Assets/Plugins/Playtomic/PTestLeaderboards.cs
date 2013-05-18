c7968398f61abc5d356a48abee9214d93bf59d08
Ben Lowry <benlowry@outlook.com>
2013-05-18 03:11:10 -0600

    Reimported from C# project

--- Assets/Plugins/Playtomic/PTestLeaderboards.cs
+++ Assets/Plugins/Playtomic/PTestLeaderboards.cs
@@ -4,304 +4,307 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Threading;
 
-//using Playtomic;
-
-//namespace PlaytomicTest
-//{
-internal class PTestLeaderboards : PTest
+internal class PTestLeaderboards : PTest 
 {	
 	public static int rnd;
-		
-	public static void FirstScore (Action done)
+	
+	public static void FirstScore(Action done) 
 	{		
-		const string section = "TestPlaytomic.Leaderboards.FirstScore";
+		const string section = "TestLeaderboards.FirstScore";
 		Debug.Log (section);
-			
-		var score = new PlayerScore {
-				table = "scores" + rnd,
-				name = "person1",
-				points = 10000,
-				highest =  true,
-				fields = new Hashtable { 
-					{"rnd", rnd}
-				}
-			};
-			
-		Playtomic.Leaderboards.Save (score, r => {
 
-			AssertTrue (section + "#1", "Request succeeded", r.success);
-			AssertEquals (section + "#1", "No errorcode", r.errorcode, 0);
+		var score = new PlayerScore {
+			table = "scores" + rnd,
+			name = "person1",
+			points = 10000,
+			highest =  true,
+			fields = new Hashtable { 
+				{"rnd", rnd}
+			}
+		};
+		
+		Playtomic.Leaderboards.Save (score, r => {
+			AssertTrue(section + "#1", "Request succeeded", r.success);
+			AssertEquals(section + "#1", "No errorcode", r.errorcode, 0);
 
 			// duplicate score gets rejected
 			score.points = 9000;
 			Thread.Sleep (1000);
 
 			Playtomic.Leaderboards.Save (score, r2 => {
-				AssertTrue (section + "#2", "Request succeeded", r2.success);
-				AssertEquals (section + "#2", "Rejected duplicate score", r2.errorcode, 209);
+				AssertTrue(section + "#2", "Request succeeded", r2.success);
+				AssertEquals(section + "#2", "Rejected duplicate score", r2.errorcode, 209);
 
 				// better score gets accepted
 				score.points = 11000;
 
 				Playtomic.Leaderboards.Save (score, r3 => {
-					AssertTrue (section + "#3", "Request succeeded", r3.success);
-					AssertEquals (section + "#3", "No errorcode", r3.errorcode, 0);
+					AssertTrue(section + "#3", "Request succeeded", r3.success);
+					AssertEquals(section + "#3", "No errorcode", r3.errorcode, 0);
 
 					// score gets accepted
 					score.points = 9000;
 					score.allowduplicates = true;
 
 					Playtomic.Leaderboards.Save (score, r4 => {
-						AssertTrue (section + "#4", "Request succeeded", r4.success);
-						AssertEquals (section + "#4", "No errorcode", r4.errorcode, 0);
-						done ();
+						AssertTrue(section + "#4", "Request succeeded", r4.success);
+						AssertEquals(section + "#4", "No errorcode", r4.errorcode, 0);
+						done();
 					});
 				});
 			});
 		});
 	}
 
-	public static void SecondScore (Action done)
+	public static void SecondScore(Action done) 
 	{		
-		const string section = "TestPlaytomic.Leaderboards.SecondScore";
+		const string section = "TestLeaderboards.SecondScore";
 		Debug.Log (section);
-		
+
 		var score = new PlayerScore {
-				table = "scores" + rnd,
-				name = "person2",
-				points = 20000,
-				allowduplicates = true,
-				highest =  true,
-				fields = new Hashtable { 
-					{"rnd", rnd}
-				}
-			};
-			
+			table = "scores" + rnd,
+			name = "person2",
+			points = 20000,
+			allowduplicates = true,
+			highest =  true,
+			fields = new Hashtable { 
+				{"rnd", rnd}
+			}
+		};
+		
 		Thread.Sleep (1000);
 		Playtomic.Leaderboards.Save (score, r => {
-			AssertTrue (section, "Request succeeded", r.success);
-			AssertEquals (section, "No errorcode", r.errorcode, 0);
-			done ();
-		});
-	}
-		
-	public static void HighScores (Action done)
-	{
-		const string section = "TestPlaytomic.Leaderboards.Highscores";
-		Debug.Log (section);
-		
-		var options = new Hashtable
-			{
-				{"table", "scores" + rnd},
-				{"highest", true},
-				{"filters", new Hashtable
-					{
-						{"rnd", rnd}
-					}
-				}
-			};
-			
-		Playtomic.Leaderboards.List (options, (scores, numscores, r) => {			
-			scores = scores ?? new List<PlayerScore> ();
-			AssertTrue (section, "Request succeeded", r.success);
-			AssertEquals (section, "No errorcode", r.errorcode, 0);
-			AssertTrue (section, "Received scores", scores.Count > 0);
-			AssertTrue (section, "Received numscores", numscores > 0);
-
-			if (scores.Count > 1) {
-				AssertTrue (section, "First score is greater than second", scores [0].points > scores [1].points);
-			} else {
-				AssertTrue (section, "First score is greater than second forced failure", false);
-			}
-
-			done ();
-		});
-	}
-		
-	public static void LowScores (Action done)
-	{
-		const string section = "TestPlaytomic.Leaderboards.LowScores";
-		Debug.Log (section);
-		
-		var options = new Hashtable
-			{
-				{"table", "scores" + rnd},
-				{"lowest", true},
-				{"perpage", 2},
-				{"filters", new Hashtable
-					{
-						{"rnd", rnd}
-					}
-				}
-			};
-			
-		Playtomic.Leaderboards.List (options, (scores, numscores, r) => {			
-			scores = scores ?? new List<PlayerScore> ();
-			AssertTrue (section, "Request succeeded", r.success);
-			AssertEquals (section, "No errorcode", r.errorcode, 0);
-			AssertTrue (section, "Received scores", scores.Count == 2);
-			AssertTrue (section, "Received numscores", numscores > 0);
-
-			if (scores.Count > 1) {
-				AssertTrue (section, "First score is less than second", scores [0].points < scores [1].points);
-			} else {
-				AssertTrue (section, "First score is less than second forced failure", false);
-			}
-
-			done ();
-		});
-	}
-		
-	public static void AllScores (Action done)
-	{
-		const string section = "TestPlaytomic.Leaderboards.AllScores";
-		Debug.Log (section);
-		
-		var options = new Hashtable
-			{
-				{"table", "scores" + rnd},
-				{"mode", "newest"},
-				{"perpage", 2}
-			};
-			
-		Playtomic.Leaderboards.List (options, (scores, numscores, r) => {			
-			scores = scores ?? new List<PlayerScore> ();
-			AssertTrue (section, "Request succeeded", r.success);
-			AssertEquals (section, "No errorcode", r.errorcode, 0);
-			AssertTrue (section, "Received scores", scores.Count > 0);
-			AssertTrue (section, "Received numscores", numscores > 0);
-
-			if (scores.Count > 1) {
-				AssertTrue (section, "First score is newer or equal to second", scores [0].date >= scores [1].date);
-			} else {
-				AssertTrue (section, "First score is newer or equal to second forced failure", false);
-			}
-
-			done ();
+			AssertTrue(section, "Request succeeded", r.success);
+			AssertEquals(section, "No errorcode", r.errorcode, 0);
+			done();
 		});
 	}
 	
+	public static void HighScores(Action done)
+	{
+		const string section = "TestLeaderboards.Highscores";
+		Debug.Log (section);
+
+		var options = new Hashtable
+		{
+			{"table", "scores" + rnd},
+			{"highest", true},
+			{"filters", new Hashtable
+				{
+					{"rnd", rnd}
+				}
+			}
+		};
+		
+		Playtomic.Leaderboards.List (options, (scores, numscores, r) => {
+			scores = scores ?? new List<PlayerScore>();
+
+			AssertTrue(section, "Request succeeded", r.success);
+			AssertEquals(section, "No errorcode", r.errorcode, 0);
+			AssertTrue(section, "Received scores", scores.Count > 0);
+			AssertTrue(section, "Received numscores", numscores > 0);
+
+			if(scores.Count > 1) {
+				AssertTrue(section, "First score is greater than second", scores[0].points > scores[1].points);
+			} else {
+				AssertTrue(section, "First score is greater than second forced failure", false);
+			}
+
+			done();
+		});
+	}
+	
+	public static void LowScores(Action done)
+	{
+		const string section = "TestLeaderboards.LowScores";
+		Debug.Log (section);
+
+		var options = new Hashtable
+		{
+			{"table", "scores" + rnd},
+			{"lowest", true},
+			{"perpage", 2},
+			{"filters", new Hashtable
+				{
+					{"rnd", rnd}
+				}
+			}
+		};
+		
+		Playtomic.Leaderboards.List (options, (scores, numscores, r) => {
+			scores = scores ?? new List<PlayerScore>();
+			AssertTrue(section, "Request succeeded", r.success);
+			AssertEquals(section, "No errorcode", r.errorcode, 0);
+			AssertTrue(section, "Received scores", scores.Count == 2);
+			AssertTrue(section, "Received numscores", numscores > 0);
+
+			if(scores.Count > 1) {
+				AssertTrue(section, "First score is less than second", scores[0].points < scores[1].points);
+			} else {
+				AssertTrue(section, "First score is less than second forced failure", false);
+			}
+
+			done();
+		});
+	}
+	
+	public static void AllScores(Action done)
+	{
+		const string section = "TestLeaderboards.AllScores";
+		Debug.Log (section);
+
+		var options = new Hashtable
+		{
+			{"table", "scores" + rnd},
+			{"mode", "newest"},
+			{"perpage", 2}
+		};
+		
+		Playtomic.Leaderboards.List (options, (scores, numscores, r) => {
+			scores = scores ?? new List<PlayerScore>();
+			AssertTrue(section, "Request succeeded", r.success);
+			AssertEquals(section, "No errorcode", r.errorcode, 0);
+			AssertTrue(section, "Received scores", scores.Count > 0);
+			AssertTrue(section, "Received numscores", numscores > 0);
+
+			if(scores.Count > 1) {
+				AssertTrue(section, "First score is newer or equal to second", scores[0].date >= scores[1].date);
+			} else {
+				AssertTrue(section, "First score is newer or equal to second forced failure", false);
+			}
+
+			done();
+		});
+	}
+
 	public static void FriendsScores(Action done)
 	{
 		const string section = "TestLeaderboards.FriendsScores";
 		Debug.Log (section);
-		
-		var playerids = new [] { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10" };
-		var points = 0;
-		var submitted = 0;
-			
-		for(var i = 0; i < 10; i++)
-		{
-			Thread.Sleep (500);
-			
-			points += 1000;
-			var playerid = playerids [i];
 
-			var score =  new PlayerScore {
-				name = "playerid" + playerid,
-				playerid = playerid,
-				table = "friends" + rnd,
-				points = points,
-				highest = true,
-				fields = {
-					{"rnd", rnd}
-				}
+		var playerids = new ArrayList(){ "1", "2", "3", "4", "5", "6", "7", "8", "9", "10" };
+
+		FriendsScoresLoop (playerids, 0, () => {
+
+			var list = new Hashtable {
+				{"table", "friends" + rnd},
+				{"perpage", 3},
+				{"friendslist", new ArrayList(new [] {"1", "2", "3" })}
 			};
 
-			Playtomic.Leaderboards.Save (score, r => {
-				
-				submitted++;
-				
-				if(submitted == 10) 
-				{
-					var list = new Hashtable {
-						{"table", "friends" + rnd},
-						{"perpage", 3},
-						{"friendslist", new ArrayList(new [] {"1", "2", "3" })}
-					};
-					
-					Playtomic.Leaderboards.List(list, (scores, numscores, r2) => {
-						scores = scores ?? new List<PlayerScore>();
-						AssertTrue(section, "Request succeeded", r2.success);
-						AssertEquals(section, "No errorcode", r2.errorcode, 0);
-						AssertTrue(section, "Received 3 scores", scores.Count == 3);
-						AssertTrue(section, "Received numscores 3", numscores == 3);
-						AssertTrue(section, "Player id #1", scores[0].playerid == "3");
-						AssertTrue(section, "Player id #2", scores[1].playerid == "2");
-						AssertTrue(section, "Player id #3", scores[2].playerid == "1");
-						done();
-					});
-				}
+			Playtomic.Leaderboards.List(list, (scores, numscores, r2) => {
+				scores = scores ?? new List<PlayerScore>();
+				AssertTrue(section, "Request succeeded", r2.success);
+				AssertEquals(section, "No errorcode", r2.errorcode, 0);
+				AssertTrue(section, "Received 3 scores", scores.Count == 3);
+				AssertTrue(section, "Received numscores 3", numscores == 3);
+				AssertTrue(section, "Player id #1", scores[0].playerid == "3");
+				AssertTrue(section, "Player id #2", scores[1].playerid == "2");
+				AssertTrue(section, "Player id #3", scores[2].playerid == "1");
+				done();
 			});
-		}
+
+		});
+	}
+
+	private static void FriendsScoresLoop(ArrayList playerids, int points, Action finished)
+	{
+		Thread.Sleep (500);
+		points += 1000;
+
+		var playerid = playerids [0].ToString ();
+		playerids.RemoveAt (0);
+
+		var score =  new PlayerScore {
+			name = "playerid" + playerid,
+			playerid = playerid,
+			table = "friends" + rnd,
+			points = points,
+			highest = true,
+			fields = {
+				{"rnd", rnd}
+			}
+		};
+
+		Playtomic.Leaderboards.Save (score, r => {
+
+			if (playerids.Count > 0) {
+				FriendsScoresLoop (playerids, points, finished);
+				return;
+			}
+	
+			finished();
+		});
 	}
 
 	public static void OwnScores(Action done)
 	{
 		const string section = "TestLeaderboards.OwnScores";
 		Debug.Log (section);
-		
-		var points = 0;
-		var submitted = 0;
 
-		for(var i=0; i<9; i++)
-		{
-			Thread.Sleep (500);
-			
-			points += 1000;
+		OwnScoresLoop (0, 0, () => {
 
-			var score = new PlayerScore {
+			var finalscore = new PlayerScore {
 				name = "test account",
 				playerid = "test@testuri.com",
 				table = "personal" + rnd,
-				points = points,
+				points = 3000,
 				highest = true,
 				allowduplicates = true,
 				fields = {
 					{"rnd", rnd}
-				}
+				},
+				perpage = 5
 			};
 
-			Playtomic.Leaderboards.Save (score, r => {
-				
-				submitted++;
-				
-				if(submitted == 9) 
-				{
-					var finalscore = new PlayerScore {
-						name = "test account",
-						playerid = "test@testuri.com",
-						table = "personal" + rnd,
-						points = 3000,
-						highest = true,
-						allowduplicates = true,
-						fields = {
-							{"rnd", rnd}
-						},
-						perpage = 5
-					};
-			
-					Playtomic.Leaderboards.SaveAndList(finalscore, (scores, numscores, r2) => {
-						scores = scores ?? new List<PlayerScore>();
-						AssertTrue(section, "Request succeeded", r2.success);
-						AssertEquals(section, "No errorcode", r2.errorcode, 0);
-						AssertTrue(section, "Received 5 scores", scores.Count == 5);
-						AssertTrue(section, "Received numscores 10", numscores == 10);
-						AssertTrue(section, "Score 1 ranked 6", scores[0].rank == 6);
-						AssertTrue(section, "Score 2 ranked 7", scores[1].rank == 7);
-						AssertTrue(section, "Score 3 ranked 8", scores[2].rank == 8);
-						AssertTrue(section, "Score 4 ranked 9", scores[3].rank == 9);
-						AssertTrue(section, "Score 5 ranked 10", scores[4].rank == 10);
-						AssertTrue(section, "Score 1 points", scores[0].points == 4000);
-						AssertTrue(section, "Score 2 points", scores[1].points == 3000);
-						AssertTrue(section, "Score 3 points", scores[2].points == 3000);
-						AssertTrue(section, "Score 4 points", scores[3].points == 2000);
-						AssertTrue(section, "Score 5 points", scores[4].points == 1000);
-						done();
-					});
-				}
+			Playtomic.Leaderboards.SaveAndList (finalscore, (scores, numscores, r2) => {
+				scores = scores ?? new List<PlayerScore> ();
+
+				AssertTrue (section, "Request succeeded", r2.success);
+				AssertEquals (section, "No errorcode", r2.errorcode, 0);
+				AssertTrue (section, "Received 5 scores", scores.Count == 5);
+				AssertTrue (section, "Received numscores 10", numscores == 10);
+				AssertTrue (section, "Score 1 ranked 6", scores [0].rank == 6);
+				AssertTrue (section, "Score 2 ranked 7", scores [1].rank == 7);
+				AssertTrue (section, "Score 3 ranked 8", scores [2].rank == 8);
+				AssertTrue (section, "Score 4 ranked 9", scores [3].rank == 9);
+				AssertTrue (section, "Score 5 ranked 10", scores [4].rank == 10);
+				AssertTrue (section, "Score 1 points", scores [0].points == 4000);
+				AssertTrue (section, "Score 2 points", scores [1].points == 3000);
+				AssertTrue (section, "Score 3 points", scores [2].points == 3000);
+				AssertTrue (section, "Score 4 points", scores [3].points == 2000);
+				AssertTrue (section, "Score 5 points", scores [4].points == 1000);
+				done ();
 			});
-		}
+		});
+	}
+
+	private static void OwnScoresLoop(int count, int points, Action finished)
+	{
+		Thread.Sleep (500);
+		points += 1000;
+		count++;
+
+		var score = new PlayerScore {
+			name = "test account",
+			playerid = "test@testuri.com",
+			table = "personal" + rnd,
+			points = points,
+			highest = true,
+			allowduplicates = true,
+			fields = {
+				{"rnd", rnd}
+			}
+		};
+
+		Playtomic.Leaderboards.Save (score, r => {
+
+			if (count < 9) {
+				OwnScoresLoop (count, points, finished);
+				return;
+			}
+
+			finished ();
+		});
 	}
 }
-//}
--- Assets/Plugins/Playtomic/PlayerScore.cs
+++ Assets/Plugins/Playtomic/PlayerScore.cs
@@ -1,113 +1,124 @@
 using System;
 using System.Collections;
 
-//namespace Playtomic
-//{
 public class PlayerScore : Hashtable
 {
-	public PlayerScore ()
+	public PlayerScore()
 	{
 	}
 
-	public PlayerScore (IDictionary data)
+	public PlayerScore(IDictionary data)
 	{
-		foreach (string x in data.Keys) {			
-			if (x == "date") {
-				var d = new DateTime (1970, 1, 1, 0, 0, 0);
-				date = d.AddSeconds ((double)data [x]);
-			} else {
-				this [x] = data [x];
+		foreach(string x in data.Keys)
+		{			
+			if(x == "date") 
+			{
+				var d = new DateTime(1970, 1, 1, 0, 0, 0);
+				date = d.AddSeconds ((double)data[x]);
+			} 
+			else 
+			{
+				this[x] = data[x];
 			}
 		}
 	}
-		
-	public string name {
+	
+	public string name 
+	{
 		get { return GetString ("name"); }
-		set { SetProperty ("name", value); }
+		set { SetProperty("name", value); }
 	}
-		
-	public string playerid {
+	
+	public string playerid
+	{
 		get { return GetString ("playerid"); }
 		set { SetProperty ("playerid", value); }
-	}
-		
-	public long points {
+	}	
+	
+	public long points
+	{
 		get { return GetLong ("points"); }
 		set { SetProperty ("points", value); }
 	}
-		
-	public string source {
+	
+	public string source
+	{
 		get { return GetString ("source"); }
 		set { SetProperty ("source", value); }
 	}
-		
-	public long rank {
+	
+	public long rank
+	{
 		get { return GetLong ("rank"); }
 		set { SetProperty ("rank", value); }
 	}
 
-	public string table {
+	public string table 
+	{
 		get { return GetString ("table"); }
 		set { SetProperty ("table", value); }
 	}
-		
-	public DateTime date {
-		get { return ContainsKey ("date") ? (DateTime)this ["date"] : DateTime.Now; }
-		set { SetProperty ("date", value); }
+	
+	public DateTime date
+	{
+		get { return ContainsKey ("date") ? (DateTime) this["date"] : DateTime.Now; }
+		private set { SetProperty ("date", value); }
 	}
 
-	public string rdate {
+	public string rdate 
+	{
 		get { return GetString ("rdate"); }
-		set { SetProperty ("rdate", value); }
 	}
 
-	public Hashtable fields {
-		get { return ContainsKey ("fields") ? (Hashtable)this ["fields"] : new Hashtable ();	}
+	public Hashtable fields
+	{
+		get { return ContainsKey ("fields") ? (Hashtable)this["fields"] : new Hashtable();	}
 		set { SetProperty ("fields", value); }
 	}
-		
-	public bool submittedorbest {
-		get { return ContainsKey ("submittedorbest"); }
-		set { SetProperty ("submittedorbest", value); }
+
+	public bool highest
+	{
+		get { return ContainsKey ("highest") && (bool) this["highest"]; }
+		set { SetProperty("highest", value); }
 	}
 
-	public bool highest {
-		get { return ContainsKey ("highest") && (bool)this ["highest"]; }
-		set { SetProperty ("highest", value); }
+	public bool lowest
+	{
+		get { return ContainsKey ("lowest") && (bool) this["lowest"]; }
+		set { SetProperty("lowest", value); }
+	}
+	
+	public bool allowduplicates
+	{
+		get { return ContainsKey ("allowduplicates") && (bool) this["allowduplicates"]; }
+		set { SetProperty("allowduplicates", value); }
 	}
 
-	public bool lowest {
-		get { return ContainsKey ("lowest") && (bool)this ["lowest"]; }
-		set { SetProperty ("lowest", value); }
-	}
-		
-	public bool allowduplicates {
-		get { return ContainsKey ("allowduplicates") && (bool)this ["allowduplicates"]; }
-		set { SetProperty ("allowduplicates", value); }
-	}
-
-	public long perpage {
+	public long perpage
+	{
 		get { return GetLong ("perpage"); }
 		set { SetProperty ("perpage", value); }
 	}
 
-	private long GetLong (string s)
+	private long GetLong(string s) 
 	{
-		return ContainsKey (s) ? long.Parse (this [s].ToString ()) : 0L;
+		return ContainsKey (s) ? long.Parse(this[s].ToString ()) : 0L;
 	}
-		
-	private string GetString (string s)
+	
+	private string GetString(string s) 
 	{	
-		return ContainsKey (s) ? this [s].ToString () : null;
+		return ContainsKey (s) ? this[s].ToString () : null;
 	}
-		
-	private void SetProperty (string key, object value)
+	
+	private void SetProperty(string key, object value) 
 	{
-		if (ContainsKey (key)) {
-			this [key] = value;
-		} else {
-			Add (key, value);
+		if(ContainsKey(key))
+		{
+			this[key] = value;
+		} 
+		else 
+		{
+			Add(key, value);
 		}
 	}
 }
-//}
